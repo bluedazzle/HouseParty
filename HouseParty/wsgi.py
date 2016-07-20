87b924dd7d8575975fd2bf4b9cd9eef5707894a7
rapospectre <rapospectre@163.com>
2016-07-20 16:08:39 +0800

    <feat>  add cron work to uwsgi
    Author:rapospectre@gmail.com
    
    Desc: add cron work to uwsgi

--- HouseParty/wsgi.py
+++ HouseParty/wsgi.py
@@ -8,7 +8,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import uwsgi
+
+from core.service import check_online
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HouseParty.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+uwsgi.register_signal(82, "", check_online)
+uwsgi.add_timer(82, 10)
