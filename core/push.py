bd358711925fbc6710801dc2bc4b32d034de9ddd
rapospectre <rapospectre@163.com>
2016-07-21 16:07:21 +0800

    <fix>  fix chartset pro in push
    Author:rapospectre@gmail.com
    
    Desc: fix chartset pro in push

--- core/push.py
+++ core/push.py
@@ -9,59 +9,72 @@ MASTER_SECRET = 'b593a0b85c902c0d2261b4d0'
 
 def push_to_friends(tag):
     _jpush = jpush.JPush(APP_KEY, MASTER_SECRET)
-    _jpush.set_logging("DEBUG")
+    _jpush.set_logging("ERROR")
+    msg = "上线啦~".encode('utf-8')
     push = _jpush.create_push()
-    ios_msg = jpush.ios(alert="上线啦~", badge="+1", extras={'k1': 'v1'})
+    ios_msg = jpush.ios(alert=msg)
     push.audience = jpush.audience(
         jpush.tag(tag, )
     )
     # push.notification = jpush.notification(alert="上线啦~", ios=ios_msg)
     push.platform = jpush.all_
     # print (push.payload)
-    push.message = jpush.message(msg_content="上线啦~", title="上线啦~", extras={'k1': 'v1'})
-    push.send()
+    push.message = jpush.message(msg_content=msg, title=msg)
+    try:
+        push.send()
+    except:
+        pass
 
 
 def push_friend_response(rid, user):
-    msg = '{0} 同意了你的好友请求'.format(user.fullname)
+    msg = '{0} 同意了你的好友请求'.format(user.fullname).encode('utf-8')
     _jpush = jpush.JPush(APP_KEY, MASTER_SECRET)
-    _jpush.set_logging("DEBUG")
+    _jpush.set_logging("ERROR")
     push = _jpush.create_push()
-    ios_msg = jpush.ios(alert=msg, badge="+1", extras={'k1': 'v1'})
+    ios_msg = jpush.ios(alert=msg)
     push.audience = jpush.audience(
         jpush.registration_id(rid)
     )
     push.notification = jpush.notification(alert=msg, ios=ios_msg)
     push.platform = jpush.all_
     # print (push.payload)
-    push.send()
+    try:
+        push.send()
+    except:
+        pass
 
 
 def push_friend_request(rid, user):
-    msg = '{0} 想添加您为好友'.format(user.fullname)
+    msg = '{0} 想添加您为好友'.format(user.fullname).encode('utf-8')
     _jpush = jpush.JPush(APP_KEY, MASTER_SECRET)
-    _jpush.set_logging("DEBUG")
+    _jpush.set_logging("ERROR")
     push = _jpush.create_push()
-    ios_msg = jpush.ios(alert=msg, badge="+1", extras={'k1': 'v1'})
+    ios_msg = jpush.ios(alert=msg)
     push.audience = jpush.audience(
         jpush.registration_id(rid)
     )
     push.notification = jpush.notification(alert=msg, ios=ios_msg)
     push.platform = jpush.all_
     # print (push.payload)
-    push.send()
+    try:
+        push.send()
+    except:
+        pass
 
 
 def push_hook(rid, user):
-    msg = '{0} 向你打招呼, 点击加入聊天'.format(user.fullname)
+    msg = '{0} 向你打招呼, 点击加入聊天'.format(user.fullname).encode('utf-8')
     _jpush = jpush.JPush(APP_KEY, MASTER_SECRET)
     _jpush.set_logging("DEBUG")
     push = _jpush.create_push()
-    ios_msg = jpush.ios(alert=msg, badge="+1", extras={'room_id': user.room.room_id})
+    ios_msg = jpush.ios(alert=msg, extras={'room_id': user.room.room_id})
     push.audience = jpush.audience(
         jpush.registration_id(rid)
     )
     push.notification = jpush.notification(alert=msg, ios=ios_msg)
     push.platform = jpush.all_
     # print (push.payload)
-    push.send()
+    try:
+        push.send()
+    except:
+        pass
