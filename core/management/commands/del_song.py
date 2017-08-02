b0427e1381a59062e7699694cfac44bb4c439f76
rapospectre <rapospectre@163.com>
2017-08-02 23:48:38 +0800

    11

--- core/management/commands/del_song.py
+++ core/management/commands/del_song.py
@@ -8,8 +8,7 @@ from core.models import Song
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        del_list = ['缺', '慢摇', '广告', '男声', '女声', '理查德', '现场版', '舞曲版', '主题曲', '铃声', '男伴奏', '女伴奏', '合作伴奏', '男同胞', '女同胞',
-                    '石进', '主题曲', '降调', '混缩', '合唱', '串烧']
+        del_list = ['11']
         for itm in Song.objects.all():
             print itm.id
             for d in del_list:
