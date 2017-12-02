1feae83ca63f0e1e92c47b3ef4a23683619d5356
zhangjianqi <zhangjianqi@bytedance.com>
2017-12-02 17:28:53 +0800

    1
    Change-Id: I6ec72f63aadcc670f903ff9c417d0079abaf8a63

---
+++ core/management/commands/active_user.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+
+from core.models import PartyUser
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for itm in PartyUser.objects.all():
+            itm.active = True
+            itm.save()
+
