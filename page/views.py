b9e121b8cc45fff52f1cf8ce7bac971b19685fbc
zhangjianqi <zhangjianqi@bytedance.com>
2017-12-16 18:54:33 +0800

    add invite page
    Change-Id: I39d6870257f840dfcb5078a039f89d64c59fa435

--- page/views.py
+++ page/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import DetailView, TemplateView
 
-from core.models import PartyUser
+from core.models import PartyUser, Room
 
 
 class InviteView(DetailView):
@@ -12,12 +12,21 @@ class InviteView(DetailView):
     model = PartyUser
 
     def get_object(self, queryset=None):
-        phone = self.request.GET.get('phone')
+        phone = self.request.GET.get('fullname')
         if phone:
-            users = PartyUser.objects.filter(phone=phone)
+            users = PartyUser.objects.filter(fullname=phone)
             if users.exists():
                 return users[0]
         return None
+
+    def get_context_data(self, **kwargs):
+        context = super(InviteView, self).get_context_data(**kwargs)
+        room_id = self.request.GET.get('room')
+        room = Room.objects.filter(room_id=room_id)
+        if room.exists():
+            room = room[0]
+            context['room'] = room
+        return context
 
 
 class GuideView(TemplateView):
