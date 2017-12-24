497f01db1f29960ca0f0990a479f6f6a1581db1b
zhangjianqi <zhangjianqi@bytedance.com>
2017-12-24 23:52:13 +0800

    update
    Change-Id: I4e6c378aff89aeecebb79d655dd731610fa471a4

--- core/management/commands/del_song.py
+++ core/management/commands/del_song.py
@@ -8,10 +8,4 @@ from core.models import Song
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        del_list = ['11']
-        for itm in Song.objects.all():
-            print itm.id
-            for d in del_list:
-                if d in itm.name:
-                    itm.delete()
-                    break
+        Song.objects.filter(hidden=True).delete()
