1fad4b9fcb703db9f5d99d0fe5d93dbcb0626ee2
zhangjianqi <zhangjianqi@bytedance.com>
2018-04-19 14:14:40 +0800

    add pw
    Change-Id: Ie9d5978ff60902d4497f72120916274944439c12

---
+++ core/management/commands/update_pwd.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+
+from core.hx import update_ease_user
+from core.models import PartyUser
+
+NEW_TOKEN = 'ZGBKrriEYvaXsiJjaQuhq5yntpl8ewWdxu7nRmTAhzSCkZGNket92Bf3dFbMIgLj'
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        fail_list = []
+        for itm in PartyUser.objects.all():
+            if update_ease_user(itm.token, NEW_TOKEN, itm.fullname):
+                print 'reset user token success'
+            else:
+                print 'reset user token failed'
+                fail_list.append(itm.fullname)
+            print 'fail list {0}'.format(fail_list)
