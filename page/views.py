b5eda13aa0afc4495b484247ee4581a4899dfb9f
zhangjianqi <zhangjianqi@bytedance.com>
2018-07-16 21:10:05 +0800

    verison
    Change-Id: I5aab632cdb6532e36c4e817fdcb962bb288f858d

--- page/views.py
+++ page/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import DetailView, TemplateView
 
-from core.models import PartyUser, Room
+from core.models import PartyUser, Room, Secret
 
 
 class InviteView(DetailView):
@@ -23,9 +23,11 @@ class InviteView(DetailView):
         context = super(InviteView, self).get_context_data(**kwargs)
         room_id = self.request.GET.get('room')
         room = Room.objects.filter(room_id=room_id)
+        secret = Secret.objects.all()[0]
         if room.exists():
             room = room[0]
             context['room'] = room
+            context['version'] = secret.version
         return context
 
 
@@ -41,6 +43,12 @@ class GuideView(DetailView):
                 return users[0]
         return None
 
+    def get_context_data(self, **kwargs):
+        context = super(GuideView, self).get_context_data(**kwargs)
+        secret = Secret.objects.all()[0]
+        context['version'] = secret.version
+        return context
+
 
 class AndroidGuideView(DetailView):
     template_name = 'guide_android.html'
@@ -54,10 +62,25 @@ class AndroidGuideView(DetailView):
                 return users[0]
         return None
 
+    def get_context_data(self, **kwargs):
+        context = super(AndroidGuideView, self).get_context_data(**kwargs)
+        secret = Secret.objects.all()[0]
+        context['version'] = secret.version
+        return context
 
-class DownloadView(TemplateView):
+
+class DownloadView(DetailView):
     template_name = 'download.html'
     http_method_names = ['get']
+
+    def get_object(self, queryset=None):
+        return None
+
+    def get_context_data(self, **kwargs):
+        context = super(DownloadView, self).get_context_data(**kwargs)
+        secret = Secret.objects.all()[0]
+        context['version'] = secret.version
+        return context
 
 
 class ServiceView(TemplateView):
