18f124b2d5b671f7d499058428ae927d91228ca1
zhangjianqi <zhangjianqi@bytedance.com>
2018-09-03 11:53:54 +0800

    d
    Change-Id: Iddf67978329f3f922450b6b2c098319e2c9c2745

--- api/urls_i18n.py
+++ api/urls_i18n.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from api import api_i18n
 
 urlpatterns = patterns('',
-                       url(r'^sms_login', api_i18n.SMSLoginView.as_view()),
-                       url(r'^verify', api_i18n.VerifyCodeView.as_view()),
+                       url(r'^sms_login/$', api_i18n.SMSLoginView.as_view()),
+                       url(r'^verify/$', api_i18n.VerifyCodeView.as_view()),
                        )
